3be1fe81e9cd1b1f6a76a65803dfe78f149aa8f1
Abdulelah <csabdulelah@hotmail.com>
2024-03-20 06:02:25 +0300

    updates

---
+++ shiba/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,3 @@
+from shiba.model import Shiba, ShibaForAutoregressiveLanguageModeling, ShibaForRegression,ShibaForClassificationD, ShibaForClassification, ShibaForSequenceLabeling, \
+    get_pretrained_state_dict, ShibaForTask
+from shiba.codepoint_tokenizer import CodepointTokenizer
